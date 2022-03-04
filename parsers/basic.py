e1956154cbd3e3d4f9a93792a99d7af39f7395b8
Daniel Ashbrook <dan@danielashbrook.com>
2022-03-04 13:35:26 +0100

    make basic parser

---
+++ parsers/basic.py
@@ -0,0 +1,14 @@
+from gcline import Line
+from util import listsplit
+
+def detect(lines):
+	return True
+
+
+def parse(gcobj):
+	"""Basic gcode parser. No preamble/postamble detection. Splits on Z."""
+	layer_class = gcobj.layer_class
+	gcobj.lines = [Line(l, lineno=n+1) for n,l in enumerate(gcobj.filelines)]
+	layers      = [layer_class(g, layernum=i) for i,g in enumerate(
+		listsplit(gcobj.lines, lambda l: 'Z' in l.line, keepsep='>', minsize=1))]
+	gcobj.layers = layers
