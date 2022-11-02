4eb30cbc2c75372f3771b34f187e722bd90513f2
Daniel Ashbrook <dan@danielashbrook.com>
2022-11-02 10:12:13 +0100

    convert gcast to decorator

--- geometry/gcast.py
+++ geometry/gcast.py
@@ -1,13 +1,21 @@
-from .gpoint import GPoint
-from .gsegment import GSegment
-from .ghalfline import GHalfLine
+from functools import wraps
 
-def gcast(obj):
+def gcastr(obj):
 	"""Cast a Geometry3D object to a G* object if one exists."""
+	from .gpoint import GPoint
+	from .gsegment import GSegment
+	from .ghalfline import GHalfLine
 	try:
-		return globals()['G' + obj.__class__.__name__](obj)
+		return locals()['G' + obj.__class__.__name__](obj)
 	except KeyError:
 		if obj is not None and not obj.__class__.__name__.startswith('G'):
-			print(f'Type G{obj.__class__.__name__} not in globals():')
-			print([k for k in globals() if k[0] != '_'])
+			print(f'Type G{obj.__class__.__name__} not in locals():')
+			print([k for k in locals() if k[0] != '_'])
 		return obj
+
+
+def gcast(f):
+	@wraps(f)
+	def wrapper(*args, **kwargs):
+		return gcastr(f(*args, **kwargs))
+	return wrapper
