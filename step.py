33450397c975c7a2a73d79e4707fb3d70377133d
Daniel Ashbrook <dan@danielashbrook.com>
2022-12-14 09:59:43 +0100

    move state restoration out of Step

--- step.py
+++ step.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 
 from geometry import GSegment, GPoint
 from geometry.utils import ang_diff
-from gcline import GCLine
+from gcline import GCLine, comment
 from util import Saver, unprinted, Number
 from logger import rprint, rich_log
 import logging
@@ -13,9 +13,11 @@ class Step:
 		self.debug      = debug
 		self.steps_obj  = steps_obj
 		self.printer = steps_obj.printer
+
+		#State
 		self.gcsegs:list[GSegment] = []
-		self.number     = -1
-		self.valid      = True
+		self.number = -1
+		self.valid = True
 		self.printer_anchor = None
 		self.ring_initial_angle = None
 		self.ring_angle = None
@@ -40,43 +42,24 @@ class Step:
 		self.gcsegs is made of GSegment objects, each of which should have a .gc_line1
 		and .gc_line2 member which are GCLines.
 		"""
-
-		gcode = []
-
 		if not self.gcsegs:
-			if self.ring_move:
-				#Variables to be restored, in the order they should be restored: we
-				# "execute" each line of ring-movement gcode to update the machine
-				# state, but want to reset the extruder to the current state after the
-				# ring moves.
-				save_vars = 'extruder_no', 'extrusion_mode', 'cold_extrusion'
-
-				newlines:list[GCLine] = []
-				with Saver(self.printer, save_vars) as saver:
-					for rline in self.printer.ring.gcode_move(self.ring_move):
-						newlines.extend(self.printer.execute_gcode(rline))
-
-				#Restore extruder state if it was changed
-				for var in saver.changed:
-					self.printer.execute_gcode(saver.saved[var])
-					newlines.append(saver.saved[var])
-
-				gcode.extend(newlines)
-
-			return gcode
+			return [] if not self.ring_move else self.printer.gcode_ring_move(self.ring_move)
 
 		#Sort gcsegs by the first gcode line number in each
 		self.gcsegs.sort(key=lambda s:s.gc_lines.first.lineno)
 
-		for seg in self.gcsegs:
+		#Set the anchor in the printer (again) so we can use it for thread location
+		# detection if needed
+		self.printer.anchor = self.printer_anchor
 
+		gcode = []
+		for seg in self.gcsegs:
 			#In a > 2-line Segment, there is always one or more X/Y Move
 			# lines, but only ever one Extrude line, which is always the last line.
 			# Save and execute every line, as the XY move will put the head in the
 			# right place for the extrude.
 			if len(seg.gc_lines) > 2:
-				for line in seg.gc_lines.data[:-1]:
-					gcode.extend(self.printer.execute_gcode(line))
+				gcode.extend(self.printer.execute_gcode(seg.gc_lines.data[:-1]))
 				extrude_line = seg.gc_lines.data[-1]
 
 			#For 2-line Segments
@@ -88,8 +71,7 @@ class Step:
 				if l1.is_xymove() and self.printer.xy != l1.xy:
 					if l1.is_xyextrude():
 						l1 = l1.as_xymove(fake=True)
-					move_lines = self.printer.execute_gcode(l1)
-					gcode.extend(move_lines)
+					gcode.extend(self.printer.execute_gcode(l1))
 
 			assert(extrude_line.is_extrude())
 			gcode.extend(self.printer.execute_gcode(extrude_line))
@@ -106,18 +88,22 @@ class Step:
 
 	def __enter__(self):
 		if self.debug is False: rich_log.setLevel(logging.CRITICAL)
+		self.ring_initial_angle = self.printer.ring.angle
 		return self
 
 
 	def __exit__(self, exc_type, value, traceback):
 		if self.debug is False: rich_log.setLevel(logging.DEBUG)
-		self.printer_anchor = self.printer.anchor.copy()
-		self.ring_angle = self.printer.ring.angle
-		self.ring_initial_angle = self.printer.ring.initial_angle
-		self.ring_move = ang_diff(self.printer.ring.initial_angle, self.printer.ring.angle)
+
+		#Save state
+		self.printer_anchor     = self.printer.anchor.copy()
+		self.ring_angle         = self.printer.ring.angle
+		self.ring_move          = ang_diff(self.ring_initial_angle, self.ring_angle)
+
 		#Die if there's an exception
 		if exc_type is not None:
 			print(f'Exception on Step.__exit__: {exc_type}')
 			return False
+
 		#Tell parent Steps object we exited
 		self.steps_obj.step_exited(self)
