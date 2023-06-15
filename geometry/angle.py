96f34ee1f22cb142b2cffea219673a6635209f3b
Daniel Ashbrook <dan@danielashbrook.com>
2023-06-15 11:10:17 +0200

    require kw args

--- geometry/angle.py
+++ geometry/angle.py
@@ -6,9 +6,11 @@ from numbers import Real
 class Angle(Real):
 	__slots__ = '_degrees', '_radians'
 
-	def __init__(self, radians=None, degrees=None):
+	def __init__(self, *, radians=None, degrees=None):
 		if degrees is not None and radians is not None:
 			raise ValueError('Must provide either degrees or radians, not both')
+		if isinstance(radians or degrees, Angle):
+			raise ValueError("Don't pass an Angle instance as an argument to Angle()!")
 
 		if degrees is not None:
 			self._degrees = degrees
@@ -20,6 +22,7 @@ class Angle(Real):
 			raise ValueError('Must provide either degrees or radians')
 
 
+	#Extract radians from objects that might be Angles
 	@classmethod
 	def _r(cls, value): return value.radians if isinstance(value, cls) else value
 
@@ -29,22 +32,21 @@ class Angle(Real):
 	@property
 	def degrees(self): return self._degrees
 
-	def __abs__(self):                 return self.__class__(abs(self.radians))
-	def __neg__(self):                 return self.__class__(-self.radians)
-	def __add__(self, other):          return self.__class__(self.radians + self._r(other))
-	def __sub__(self, other):          return self.__class__(self.radians - self._r(other))
-	def __mul__(self, other):          return self.__class__(self.radians * self._r(other))
-	def __truediv__(self, other):      return self.__class__(self.radians / self._r(other))
+	def __abs__(self):                 return self.__class__(radians=abs(self.radians))
+	def __neg__(self):                 return self.__class__(radians=-self.radians)
+	def __add__(self, other):          return self.__class__(radians=self.radians + self._r(other))
+	def __sub__(self, other):          return self.__class__(radians=self.radians - self._r(other))
+	def __mul__(self, other):          return self.__class__(radians=self.radians * self._r(other))
+	def __truediv__(self, other):      return self.__class__(radians=self.radians / self._r(other))
+	def __mod__(self, other):          return self.__class__(radians=self.radians % self._r(other))
 	def __radd__(self, other):         return self.__class__.__add__(self, other)
 	def __rsub__(self, other):         return self.__class__.__add__(-self, other)
 	def __rmul__(self, other):         return self.radians * other
 	def __rtruediv__(self, other):     return other / self.radians
 	def __floordiv__(self, other):     return self.radians // self._r(other)
-	def __mod__(self, other):          return self.radians % other
 
 	def __hash__(self):                return hash(self.radians)
 	def __float__(self):               return float(self.radians)
-	def __str__(self):                 return str(self.radians)
 
 	def __eq__(self, other):           return self.radians == other
 	def __ne__(self, other):           return self.radians != other
@@ -56,7 +58,7 @@ class Angle(Real):
 	def __floor__(self):               return floor(self.radians)
 	def __ceil__(self):                return ceil(self.radians)
 
-	def __pos__(self):                 return self.__class__(+self.radians)
+	def __pos__(self):                 return self.__class__(radians=+self.radians)
 	def __pow__(self, value):          return pow(self.radians, value)
 	def __rfloordiv__(self, other):    return other // self.radians
 	def __rmod__(self, other):         return other % self.radians
@@ -64,11 +66,13 @@ class Angle(Real):
 	def __rpow__(self, base):          return NotImplemented
 	def __trunc__(self):               return trunc(self.radians)
 
-	def __repr__(self): return f'{self.degrees:.3f}°'
+	def __str__(self):                 return f'{self.degrees:.3f}°'
+	def __repr__(self):                return f'{self.degrees:.3f}°'
+	def __format__(self, spec):        return format(self.degrees, spec)
 
 
 
 #Redefine trigonometric functions to return Angle objects
-def atan2(y, x): return Angle(_atan2(y, x))
-def acos(x):     return Angle(_acos(x))
-def asin(x):     return Angle(_asin(x))
+def atan2(y, x): return Angle(radians=_atan2(y, x))
+def acos(x):     return Angle(radians=_acos(x))
+def asin(x):     return Angle(radians=_asin(x))
