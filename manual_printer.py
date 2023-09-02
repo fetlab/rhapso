126283b2042c7d87f33e6c85eec759fb91750874
Daniel Ashbrook <dan@danielashbrook.com>
2023-09-02 22:16:36 +0200

    basic manual printer

--- manual_printer.py
+++ manual_printer.py
@@ -4,5 +4,10 @@ from gcline import GCLine
 
 class ManualPrinter(GCodePrinter):
 
-  def gcode_set_thread_path(self, thread_path, target) -> list[GCLine]:
-  	"""Return code to pause the print and display a message about where the thread should be moved to next."""
+	def gcode_set_thread_path(self, thread_path, target) -> list[GCLine]:
+		"""Return code to pause the print and display a message about where the
+		thread should be moved to next."""
+		return [
+				GCLine(code='M117', args={None: f'Move thread to {thread_path.angle}'}),
+				GCLine(code='M601', comment='Pause, wait for user to click buttton')
+				]
